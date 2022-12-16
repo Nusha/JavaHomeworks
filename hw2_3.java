86672fa50eba497b4649706f6b8f21591bc97dc5
Ivan Balabay <i.balabai@gmail.com>
2022-12-16 07:35:34 +0300

    Deparsing warped in method as you whiled )

--- hw2_3.java
+++ hw2_3.java
@@ -1,14 +1,24 @@
 import java.util.*;
 
 public class hw2_3 {
-    public static void main(String[] args) {
-        String s = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"},{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
+        static String[] deparse(String string) {
         String regex = "[\\[ \\] \\{ \\} \\: \"]";
-        s = s.replace("предмет","");
-        s = s.replace("фамилия","");
-        s = s.replace("оценка","");
-        s = s.replaceAll(regex, "");
-        String[] arr = s.split(("\\,"));
+        string = string.replace("предмет","");
+        string = string.replace("фамилия","");
+        string = string.replace("оценка","");
+        string = string.replaceAll(regex, "");
+        String[] arr = string.split(("\\,"));
+        return arr;
+    };
+    public static void main(String[] args) {
+          String s = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"},{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
+//        String regex = "[\\[ \\] \\{ \\} \\: \"]";
+//        s = s.replace("предмет","");
+//        s = s.replace("фамилия","");
+//        s = s.replace("оценка","");
+//        s = s.replaceAll(regex, "");
+//        String[] arr = s.split(("\\,"));
+        String[] arr = deparse(s);
         List<String> surnames = new ArrayList<>();
         List<String> grades = new ArrayList<>();
         List<String> disciplines = new ArrayList<>();
@@ -19,9 +29,6 @@ public class hw2_3 {
             grades.add(arr[i+1]);
             disciplines.add(arr[i+2]);
         }
-//        System.out.println(surnames);
-//        System.out.println(grades);
-//        System.out.println(disciplines);
         for (int i = 0; i < surnames.size(); i++) {
             StringBuilder sb = new StringBuilder(tmplt.get(0)+" "+surnames.get(i)+" "+tmplt.get(1)+" "+grades.get(i)+" "+tmplt.get(2)+" "+disciplines.get(i)+".");
             System.out.println(sb);
