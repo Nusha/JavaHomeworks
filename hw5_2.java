d98028cced2e03cae89b4aa59a977acf9a6852d7
Ivan Balabay <i.balabai@gmail.com>
2022-12-25 23:00:31 +0300

    Changes and fixes in code.

--- hw5_2.java
+++ hw5_2.java
@@ -4,15 +4,9 @@ public class hw5_2 {
     public static HashMap<String, Integer> sortMap(HashMap<String, Integer> hm)
     {
         List<Map.Entry<String, Integer> > list =
-                new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
-                return (o2.getValue()).compareTo(o1.getValue());
-            }
-        });
-        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+                new LinkedList<>(hm.entrySet());
+        list.sort((o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
+        HashMap<String, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<String, Integer> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
         }
@@ -34,22 +28,18 @@ public class hw5_2 {
         printLst(nlt);
         System.out.println("*******************");
         List<String> onln = new ArrayList<>();
-        for (int i = 0; i < nlt.size(); i++) {
-            onln.add(nlt.get(i).split(" ") [0]);
+        for (String s : nlt) {
+            onln.add(s.split(" ")[0]);
         }
 
-        Map<String,Integer> names = new HashMap<String,Integer>();
-        for (int i = 0; i < onln.size(); i++) {
-            String value = onln.get(i);
-             if (names.get(value) != null) names.put(value, names.get(value) + 1);
-             else names.put(value, 1);
-             }
+        HashMap<String, Integer> names = new HashMap<>();
+        for (String value : onln) {
+            names.merge(value, 1, Integer::sum);
+        }
 
-        Map<String,Integer> sortednames = sortMap((HashMap<String, Integer>) names);
+        Map<String,Integer> sortednames = sortMap(names);
         System.out.println("The result: ");
-        sortednames.forEach((n, e) -> {
-            System.out.println("Name " + n + " has " + e + " entries.");
-        });
+        sortednames.forEach((n, e) -> System.out.println("Name " + n + " has " + e + " entries."));
         }
         }
 
